5e659fe6d1c05d248c313833b0e37b9c2dd06942
Anthony Longo <ferrari13@me.com>
2018-09-02 15:42:37 -0400

    added os commands to lab2.py

--- lab2.py
+++ lab2.py
@@ -1,4 +1,4 @@
-
+import os
 def main():
     weeks = int(input("How many weeks: "))
     print(type(weeks))
@@ -9,5 +9,16 @@ def main():
     cost_per_week = ((tuition / classes) / weeks)
     print("Cost per week:", cost_per_week)
     print(type(cost_per_week))
+
+    os.system("man man")
+    os.system("man cat")
+    os.system("man pwd")
+    os.system("man mkdir")
+    os.system("man rm")
+    os.system("man cd")
+    os.system("man ls")
+    os.system("man mv")
+    os.system("man cp")
+    os.system("man python2")
 
 main()
