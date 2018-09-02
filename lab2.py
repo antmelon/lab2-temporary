8b49eb4c4b8ba36059bdb38b2f51a8877c74a778
antmelon <ferrari13@me.com>
2018-09-02 13:48:26 -0400

    edited lab2.py

--- lab2.py
+++ lab2.py
@@ -1,9 +1,13 @@
 
 def main():
-    weeks = 16
-    classes = 5
-    tuition = 5000
+    weeks = int(input("How many weeks: "))
+    print(type(weeks))
+    classes = int(input("How many classes: "))
+    print(type(classes))
+    tuition = int(input("How much is tuition: "))
+    print(type(tuition))
     cost_per_week = ((tuition / classes) / weeks)
     print("Cost per week:", cost_per_week)
+    print(type(cost_per_week))
 
 main()
